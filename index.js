e9a386fd2dfd6ce74d2c72b680734e088a60a1d9
BeckiGeanny <rebekka.wollmeister@gmail.com>
2022-07-30 20:30:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// let username = "Becks";
+// let edad = 24;
+
+// if (username == "Becks") {console.log ("Hello", username)}
+// if (edad == 24) {console.log ("You are", edad)}
+
+// let username = prompt("Enter your name:")
+
+// if (username=="Becks") {console.log("Hello", username)} else{ console.warn("Not recocgnising this user")}
+
+// alert("Attention");
+// confirm("ACCEPT");
+
+// debugger
+
+// let miOferta = parseInt(prompt("Ingrese su oferta"))
+
+// if (miOferta<20) {
+//     console.log("Too Low")
+// }
+// else if(miOferta<30){
+//     console.warn("Go higher")
+// }
+
+// else if(miOferta>=80){
+//     console.log("done")
+// }
+
+// debugger
+
+// let username = prompt("Ingrese su nombre")
+// let password = prompt("Ingrese su password")
+
+// if (username =="Bety" && password == "12345") {
+//     console.log("welcome", username)
+// } else {
+//     console.log("User not found")
+// }
+
+// let limite = parseInt(prompt("Ingresa un número:"))
+
+// ciclos
+
+for (let i = 0; i < 16; i++) {
+    console.log("Iteración Nro.", i)
+}
+debugger 
+
+let limite = parseInt(prompt("Ingrese su numero"))
+
+for (let i = 0; i <limite; i++) {
+    console.log("Iteración Nro.", i)
+}
