e1ac6530cd4c6c52f33d68efe09f4871199211ca
BeckiGeanny <rebekka.wollmeister@gmail.com>
2022-08-25 23:36:59 -0300

    segunda entrega

---
+++ segunda-entrega/desafio.js
@@ -0,0 +1,28 @@
+const buttonContact = document.querySelector(".contact-us")
+const emailInputDiv = document.getElementById("contactField")
+const emailInputField = document.getElementById("emailInput")
+const emailSendBtn = document.getElementById("sendEmailBtn")
+const responseToContact = document.getElementById("responseToContact")
+
+buttonContact.addEventListener("click", () => showEmailInput())
+emailSendBtn.addEventListener("click", () => storeEmailClick())
+emailInputField.addEventListener("keydown", (e) => storeEmail(e))
+
+
+let showEmailInput = () => {
+    emailInputDiv.style.display = "block"
+}
+
+let storeEmail = (e) => {
+    if (e.key === "Enter") {
+        contactEmail = emailInputField.value
+        console.log(contactEmail)
+        responseToContact.innerText = `Keep an eye on your email: ${contactEmail}`
+    }
+}
+
+let storeEmailClick = () => {
+    contactEmail = emailInputField.value
+    console.log(contactEmail)
+    responseToContact.innerText = `Keep an eye on your email: ${contactEmail}`
+}
